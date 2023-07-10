5c4a83cdc6f5e141831b7acadfc529b21c7317e7
st177975 <st177975@stud.uni-stuttgart.de>
2023-07-10 11:08:09 +0200

    created functions for both data analyzers

--- Camera_PointCloud/pointCloudReader.py
+++ Camera_PointCloud/pointCloudReader.py
@@ -5,7 +5,8 @@ import open3d as o3d
 def convert_to_pcd(filename_without_type, xyz_only=False):
     pcd_file_header = "# .PCD v0.7 - Point Cloud Data file format\nVERSION 0.7\nFIELDS x y z\nSIZE 4 4 4\nTYPE F F F\nCOUNT 1 1 1\nWIDTH 251\nHEIGHT 1\nVIEWPOINT 0 0 0 1 0 0 0\nPOINTS 251\nDATA ascii"
 
-    y = np.genfromtxt(f"data/{filename_without_type}.txt", delimiter=',', dtype=np.float32)
+    y = np.genfromtxt(f"data/{filename_without_type}.txt",
+                      delimiter=',', dtype=np.float32)
 
     # optionally transform to xyz only (without rgb)
     if xyz_only:
@@ -15,12 +16,20 @@ def convert_to_pcd(filename_without_type, xyz_only=False):
                fmt="%1.5f", comments="", header=pcd_file_header)
 
 
-if __name__ == "__main__":
-    filename_without_type = "PointCloud_14-40-51"
-    convert_to_pcd(filename_without_type)
-
+def open_point_cloud(filename_without_type, print_details=False):
+    # load point cloud
     pcd = o3d.io.read_point_cloud(f"data/{filename_without_type}.pcd")
 
-    print(pcd)
+    # optionally print details about the loaded point cloud
+    if print_details:
+        print(pcd)
 
     o3d.visualization.draw_geometries([pcd])
+
+
+if __name__ == "__main__":
+    filename_without_type = "PointCloud_14-40-51"
+
+    convert_to_pcd(filename_without_type)
+
+    open_point_cloud(filename_without_type)
--- IMU_Reader/show_example_data.py
+++ IMU_Reader/show_example_data.py
@@ -1,10 +1,24 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-if __name__ == "__main__":
-    y = np.genfromtxt('logged_data/footForce.csv', delimiter=';')
-    #y = y[2000:2500]
+
+def plot_IMU_data(measurement_name, sensor_name, start_of_plot=None, end_of_plot=None):
+    # load data
+    y = np.genfromtxt(f'{measurement_name}/{sensor_name}.csv', delimiter=';')
+
+    # select wanted part of the measurement
+    if start_of_plot != None and end_of_plot != None:
+        y = y[start_of_plot:end_of_plot]
+
     x = np.arange(0, np.shape(y)[0])
-    plt.title("Matplotlib demo") 
-    plt.plot(x,y) 
+    plt.title("Matplotlib demo")
+    plt.plot(x, y)
     plt.show()
+
+
+if __name__ == "__main__":
+    measurement_name = "first_dance"
+    # different measurements names to copy: accelerometer, bodyHeight, footForce, footRaiseHeight, gyroscope, mode, rpy, temperature, velocity, yawSpeed
+    sensor = "footForce"
+
+    plot_IMU_data(measurement_name, sensor)
