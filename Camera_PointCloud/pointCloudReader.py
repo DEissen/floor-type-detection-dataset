bc7d02f1bfe44c57ea52f2aa3d16406c33d80fa6
st177975 <st177975@stud.uni-stuttgart.de>
2023-07-12 13:57:44 +0200

    upgrade point cloud visualization with rgb support, ...

--- Camera_PointCloud/pointCloudReader.py
+++ Camera_PointCloud/pointCloudReader.py
@@ -1,32 +1,54 @@
 import numpy as np
 import open3d as o3d
 
+# not needed for new measurement anymore, as data is stored directly in correct format in the meantime
 
-def convert_to_pcd(filename_without_type, xyz_only=False):
-    y = np.genfromtxt(f"kurz_vor_Heizung/{filename_without_type}.txt",
+
+def convert_txt_to_pcd(measurement_name, filename_without_type, xyz_only=False, add_direction_points=False):
+    y = np.genfromtxt(f"{measurement_name}/{filename_without_type}.txt",
                       delimiter=',', dtype=np.float32)
 
     # optionally transform to xyz only (without rgb)
     if xyz_only:
         y = y[:, :3]
 
-    # remove nan
+    # remove nan values if some are present
     y = y[~np.isnan(y).any(axis=1)]
+
+    # add points to indicate directions by adding the following four points to the point cloud:
+    # 0 0 0 4.2108e+01      => reference point in origin in darkblue
+    # 0.1 0 0 4.2108e+01    => reference point at x = 0.1 in darkblue
+    # 0 0.1 0 4.2108e+024   => reference point at y = 0.1 in lightblue
+    # 0 0 0.1 0             => reference point at z = 0.1 in black
+    if add_direction_points:
+        darkblue = 4.2108e+01
+        lightblue = 4.2108e+024
+        black = 0
+        y = np.append(y, [[0, 0, 0, darkblue], [0.1, 0, 0, darkblue], [
+                      0, 0.1, 0, lightblue], [0, 0, 0.1, black]], axis=0)
 
     # prepare pcd header with content type (with or without rgb) and length
     length = np.shape(y)[0]
-    # rgb = " rgb" # with rgb is not working yet
-    # if xyz_only:
-    #     rgb = ""
-    pcd_file_header = f"# .PCD v0.7 - Point Cloud Data file format\nVERSION 0.7\nFIELDS x y z\nSIZE 4 4 4\nTYPE F F F\nCOUNT 1 1 1\nWIDTH {length}\nHEIGHT 1\nVIEWPOINT 0 0 0 1 0 0 0\nPOINTS {length}\nDATA ascii"
+    rgb = " rgb"  # with rgb is not working yet
+    rgb_size = " 4"
+    rgb_type = " F"
+    rgb_count = " 1"
+    if xyz_only:
+        rgb = ""
+        rgb_size = ""
+        rgb_type = ""
+        rgb_count = ""
 
-    np.savetxt(f"data/{filename_without_type}.pcd", y, delimiter=" ",
-               fmt=["%1.7f", "%1.7f", "%1.7f", "%d"], comments="", header=pcd_file_header)
+    pcd_file_header = f"# .PCD v0.7 - Point Cloud Data file format\nVERSION 0.7\nFIELDS x y z{rgb}\nSIZE 4 4 4{rgb_size}\nTYPE F F F{rgb_type}\nCOUNT 1 1 1{rgb_count}\nWIDTH {length}\nHEIGHT 1\nVIEWPOINT 0 0 0 1 0 0 0\nPOINTS {length}\nDATA ascii"
+
+    np.savetxt(f"{measurement_name}/{filename_without_type}.pcd", y, delimiter=" ",
+               fmt=["%.10f", "%.10f", "%.10f", "%.10e"], comments="", header=pcd_file_header)
 
 
-def open_point_cloud(filename_without_type, print_details=False):
+def open_point_cloud(measurement_name, filename_without_type, print_details=False):
     # load point cloud
-    pcd = o3d.io.read_point_cloud(f"data/{filename_without_type}.pcd")
+    pcd = o3d.io.read_point_cloud(
+        f"{measurement_name}/{filename_without_type}.pcd")
 
     # optionally print details about the loaded point cloud
     if print_details:
@@ -36,8 +58,10 @@ def open_point_cloud(filename_without_type, print_details=False):
 
 
 if __name__ == "__main__":
-    filename_without_type = "PointCloud_16-36-58"
+    measurement_name = "weiter_weg"
+    filename_without_type = "PointCloud_16-37-40"
 
-    convert_to_pcd(filename_without_type)
+    convert_txt_to_pcd(measurement_name, filename_without_type,
+                   add_direction_points=True)
 
-    open_point_cloud(filename_without_type, True)
+    open_point_cloud(measurement_name, filename_without_type, True)
