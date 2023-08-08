d7118b1cefec8a55e2ffff037ff850b51f105b50
st177975 <st177975@stud.uni-stuttgart.de>
2023-08-08 17:45:48 +0200

    updated data_prepataion_main.py after last changes

--- data_preparation_main.py
+++ data_preparation_main.py
@@ -1,10 +1,12 @@
 import os
 
 # custom imports
-from custom_utils.utils import copy_measurement_to_temp, clean_temp_dir
+from custom_utils.utils import copy_measurement_to_temp, clean_temp_dir, load_complete_IMU_measurement
 from data_preparation.timestamp_evaluation import get_synchronized_timestamps
-from data_preparation.timeseries_preparation import TimeseriesDownsamplingForWholeMeasurement
+from data_preparation.timeseries_preparation import TimeseriesDownsamplingForWholeMeasurement, remove_obsolete_values
 from data_preparation.image_preparation import remove_obsolete_images
+from visualization.visualizeTimeseriesData import plot_IMU_data
+from visualization.visualizeImages import show_all_images_afterwards, show_all_images_afterwards_including_imu_data
 
 
 def main(measurement_path):
@@ -24,12 +26,24 @@ def main(measurement_path):
 
     for key, timestamp in timestamps.items():
         print(f"Starting timestamp for {key} is {timestamp}")
-        if "Cam" in key:
+        if "IMU" in key:
+            for sensor in timeseries_downsampler.timeseries_sensors:
+                remove_obsolete_values(temp_path, sensor, timestamp)
+        elif "Cam" in key:
             remove_obsolete_images(temp_path, key, timestamp)
 
+def visualize_intermediate_result():
+    file_dir = os.path.dirname(os.path.abspath(__file__))
+    temp_path = os.path.join(file_dir, "temp")
+
+    data = load_complete_IMU_measurement(temp_path, "accelerometer") # determines which sensor is used for visualization
+    show_all_images_afterwards_including_imu_data(temp_path, data)
+    plot_IMU_data(data)
 
 
 if __name__ == "__main__":
     measurement_path = "./testdata/measurement_25_07__15_03"
 
     main(measurement_path)
+
+    visualize_intermediate_result()
