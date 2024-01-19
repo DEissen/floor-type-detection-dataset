f93981a27dc286b7fdf0cce601c8a6e70f771375
Dominik <dome.dome1@posteo.de>
2024-01-19 14:55:48 +0100

    Added measurement combination to final dataset

--- data_preparation_main.py
+++ data_preparation_main.py
@@ -9,6 +9,7 @@ from data_preparation.timestamp_evaluation import get_synchronized_timestamps, r
 from data_preparation.timeseries_preparation import TimeseriesDownsamplingForWholeMeasurement, remove_obsolete_values, load_complete_IMU_measurement, create_sliding_windows_and_save_them
 from data_preparation.image_preparation import remove_obsolete_images_at_beginning, unify_image_timestamps
 from data_preparation.incomplete_data_cleanup import get_incomplete_data_samples, delete_incomplete_data_samples, update_labels_csv
+from data_preparation.measurement_combination import combine_measurements_to_dataset
 from visualization.visualizeTimeseriesData import plot_IMU_data
 from visualization.visualizeImages import show_all_images_afterwards, show_all_images_afterwards_including_imu_data
 from data_preprocessing_main import data_preprocessing_main
@@ -130,7 +131,7 @@ def data_preparation_main(measurement_path, temp_path=None, dataset_path=None, w
     # visualize_result(window_size)
 
 
-def visualize_result(imu_offset=0):
+def visualize_result(imu_offset=0, temp_path=None):
     """
         Helper function to load and visualize data from one IMU sensor and one picture of each stereo camera present in temp/ dir.
         Can be used the show e.g. intermediate results.
@@ -138,8 +139,9 @@ def visualize_result(imu_offset=0):
         Parameters:        
             - imu_offset (int): Offset for vertical line in IMU plot (needed if sliding windows were already created)
     """
-    file_dir = os.path.dirname(os.path.abspath(__file__))
-    temp_path = os.path.join(file_dir, "temp")
+    if temp_path == None:
+        file_dir = os.path.dirname(os.path.abspath(__file__))
+        temp_path = os.path.join(file_dir, "temp")
 
     # determines which sensor is used for visualization
     data = load_complete_IMU_measurement(temp_path, "accelerometer")
@@ -163,10 +165,11 @@ if __name__ == "__main__":
 
     ######## create dataset from multiple measurements
     measurement_base_path = r"C:\Users\Dominik\Downloads\test_data"
+    final_dataset_path = r"C:\Users\Dominik\Downloads\test_combination\FTDD_2.0_test"
     file_dir = os.path.dirname(os.path.abspath(__file__))
     result_dir = os.path.join(file_dir, "results")
 
-    # clean result and temp dir at the start
+    # # clean result and temp dir at the start
     print("### Step 0: Clean temp and results dirs ###")
     clean_results_dir()
     clean_temp_dir()
@@ -187,3 +190,6 @@ if __name__ == "__main__":
                 measurement_path, dataset_path=measurement_result_dir, temp_path=measurement_temp_dir)
         # break after first for loop to only explore the top level of measurement_base_path
         break
+
+    # combine measurements to dataset at the end
+    combine_measurements_to_dataset(result_dir, final_dataset_path)
